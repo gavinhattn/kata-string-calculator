8982255e156472583e5e0adb07af6b7d5048f939
Gavin Hatton <gavin.hatton@boohoo.com>
2026-03-10 10:07:13 +0000

    Initial setup and add empty string returns 0 test

---
+++ src/main/java/com/kata/stringcalculator/StringCalculator.java
@@ -0,0 +1,8 @@
+package com.kata.stringcalculator;
+
+public class StringCalculator {
+
+    public int add(String numbers) {
+        return 0;
+    }
+}
---
+++ src/test/java/com/kata/stringcalculator/StringCalculatorTest.java
@@ -0,0 +1,23 @@
+package com.kata.stringcalculator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class StringCalculatorTest {
+
+    private StringCalculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new StringCalculator();
+    }
+
+    @Test
+    @DisplayName("Empty string returns 0")
+    void emptyStringReturnsZero() {
+        assertThat(calculator.add("")).isZero();
+    }
+}
